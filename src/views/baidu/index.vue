db3f792d760cac38c6fbd2408f6377452f1bec8d
liuying <15526498331@163.com>
2020-03-20 17:21:34 +0800

    first-commit

---
+++ src/views/baidu/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <baidu-map class="map" center="长沙" ak="RFe9oUcSqrLcuCgTlL6Ncs8y0V77BgNW">
+  </baidu-map>
+</template>
+
+<script>
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
+export default {
+  components: {
+    BaiduMap
+  }
+}
+</script>
+
+<style scoped>
+  .map {
+    width: 100%;
+    height: 900px;
+  }
+</style>
